513475170c87ad2f6599946d516a4af1d0918d88
manishaa8981 <manishaa8981@gmail.com>
2025-07-13 22:38:41 +0545

    app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import ContactUs from "./components/ContactUs";
-import Failure from "./components/Failure";
 import PaymentForm from "./components/PaymentForm";
-import Success from "./components/Success";
 import VideoCarouselBasicExample from "./components/VideoCarousel";
 import AboutUs from "./pages/AboutUs";
 import Cart from "./pages/Cart";
@@ -11,6 +9,7 @@ import CheckoutPage from "./pages/CheckoutPage";
 import FAQPage from "./pages/FAQ";
 import ForgotPasswordPage from "./pages/ForgetPassword";
 import Homepage from "./pages/Home";
+import KhaltiSuccessPage from "./pages/KhaltiSuccessPage";
 import Login from "./pages/Login";
 import OurMission from "./pages/OurMission";
 import ProductDetail from "./pages/ProductDetail";
@@ -18,6 +17,7 @@ import Products from "./pages/Products";
 import Register from "./pages/Register";
 import ResetPasswordPage from "./pages/ResetPassword";
 import Wishlist from "./pages/Wishlist";
+
 const App = () => (
   <BrowserRouter>
     <Routes>
@@ -36,8 +36,8 @@ const App = () => (
       <Route path="/faq" element={<FAQPage />} />
       <Route path="/aboutus" element={<AboutUs />} />
       <Route path="/contactus" element={<ContactUs />} />
-      <Route path="/payment-success" element={<Success />} />
-      <Route path="/payment-failure" element={<Failure />} />
+      <Route path="/khalti-success" element={<KhaltiSuccessPage />} />
+
       <Route path="/videcoarousel" element={<VideoCarouselBasicExample />} />
     </Routes>
   </BrowserRouter>
