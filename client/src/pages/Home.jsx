0753d1e4b8c1e92f623a10f22fd766da02e9fb8b
manishaa8981 <manishaa8981@gmail.com>
2025-07-19 01:04:27 +0545

    home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,6 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 import { FaQuestionCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import CategoryGrid from "../components/categoryGrid";
@@ -9,14 +12,37 @@ import RatnajyotiWorld from "../components/ratnajyotiWorld";
 import VideoCarouselBasicExample from "../components/VideoCarousel";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800, // animation duration (ms)
+      once: true, // whether animation should happen only once
+      easing: "ease-in-out",
+    });
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <VideoCarouselBasicExample />
-      <CollectionsShowcase />
-      <CategoryGrid />
-      <RatnajyotiWorld />
-      <NewArrivals />
+      <div>
+        <VideoCarouselBasicExample />
+      </div>
+
+      <div data-aos="zoom-in">
+        <CollectionsShowcase />
+      </div>
+
+      <div data-aos="fade-up">
+        <CategoryGrid />
+      </div>
+
+      <div data-aos="fade-left">
+        <RatnajyotiWorld />
+      </div>
+
+      <div data-aos="fade-right">
+        <NewArrivals />
+      </div>
+
       <Link
         to="/faq"
         className="fixed bottom-6 right-6 z-50 bg-[#b6845b] hover:bg-[#a06d48] text-white p-4 rounded-full shadow-lg transition-all duration-300 flex items-center justify-center"
